705f85cc156b5fabac8e486e3ec115372462648f
zduissekov <zduissekov@beeline.kz>
2023-05-03 07:53:22 +0300

    refactor repo

--- internal/repository/entity.go
+++ internal/repository/entity.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"github.com/zhenisduissekov/http-3rdparty-task-service/internal/config"
 	"github.com/zhenisduissekov/http-3rdparty-task-service/internal/entity"
 )
 
@@ -20,4 +21,20 @@ func (a *Task) converterToRepository(items entity.Task) {
 	a.ReqBody = items.ReqBody
 	a.Status = items.Status
 	a.Headers = items.Headers
+}
+
+
+type Storage interface {
+	Set(key string, value entity.Task)
+	Get(key string) (entity.Task, error)
+}
+
+type Repository struct {
+	Storage
+}
+
+func NewRepository(cnf *config.Conf) *Repository {
+	return &Repository{
+		Storage: NewCache(cnf),
+	}
 }
